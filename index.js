819710d51092d69c7b99a037ead2dabd545d2a50
Haripriya Chittari <hpvarma09@gmail.com>
2022-11-10 20:14:48 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+var readlineSync=require('readline-sync');
+
+
+var questionsArr = [
+   { que: "Who is You-know-who?", rightAns: 'voldemort' },
+  { que: "In what house did sorting hat almost put harry in ?", rightAns: 'slytherin' },
+ 
+  { que: "who is harry's friend?", rightAns: 'ron' },
+  { que: "how did harry and ron get to hogwarts in their second year?", rightAns: 'flyingcar' },
+  { que: "who is the principal of hogwarts?", rightAns:'dumbledore' },
+];
+var topScores = [
+  { name: 'Krishna', score: 5 },
+  { name: 'Tanmay', score: 4 },
+];
+
+function startGameAndGetScore(ourUser) {
+  var score = 0;
+  for (var i = 0; i < questionsArr.length; i++) {
+    var userAns = readlineSync.question(questionsArr[i].que);
+    if (userAns.toLowerCase() === questionsArr[i].rightAns) {
+      score += 1;
+      console.log(`✅ Correct`);
+    } else {
+      console.log(`❌ Wrong !!`);
+    }
+
+    console.log('Your current score is ' + score);
+   enter();
+    dash();
+  }
+
+  return { name: ourUser, score };
+}
+
+function enter() {
+  console.log('\n');
+}
+function dash() {
+  console.log('-------------------------------------------------');
+}
+
+function showHighScore() {
+  topScores.sort((a, b) => b.score - a.score);
+  dash();
+  console.log(
+    '                 ➡  LEADERBOARD ⬅                      '
+  );
+  topScores.forEach((singleData) =>
+    console.log(singleData.name + ' : ' +singleData.score));
+  dash();
+}
